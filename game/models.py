1e3a2c44c5d8fc81d6d6029474945dee80d5d0bd
RakibCSE <rakibul.hossain365@gmail.com>
2019-09-17 01:05:31 +0600

    category and game model implemented

--- game/models.py
+++ game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to="image/")
+    category = models.ForeignKey(
+        Category, related_name="category", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
