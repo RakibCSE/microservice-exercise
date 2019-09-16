5a29fa6863cdc7828c565aabe0867bf4cb89c6d2
RakibCSE <rakibul.hossain365@gmail.com>
2019-09-17 01:06:17 +0600

    categories and games url added

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'),
-]
+    path('api/categories/', views.CategoryList.as_view(), name='category-list'),
+    path('api/games/', views.GameList.as_view(), name='game-list'),
+    path('api/game/<int:pk>', views.GameDetail.as_view(), name='game-detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
